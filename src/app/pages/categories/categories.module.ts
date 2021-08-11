07b4cca36dee044fb8acfdc9238ba6fc764af2b9
luizpedro.junior <luizpedro.junior@dataprev.gov.br>
2021-08-11 14:51:20 -0300

    finalizacao do formulario

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 
-
 @NgModule({
-  declarations: [CategoryFormComponent, CategoryListComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
-  ]
+    CategoriesRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [CategoryFormComponent, CategoryListComponent]
 })
 export class CategoriesModule { }
