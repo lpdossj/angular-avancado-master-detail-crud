57f3feab191a016f555e51607b1e48bcbe5d3f6c
luizpedro.junior <luizpedro.junior@dataprev.gov.br>
2021-08-26 15:47:54 -0300

    improving reports

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -9,7 +9,7 @@ export class InMemoryDatabase implements InMemoryDbService {
     
     createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
         const categories: Category[] = [
-            { id: 1, name: "Maradia", description: "Pagamento de contas da casa"},
+            { id: 1, name: "Moradia", description: "Pagamento de contas da casa"},
             { id: 2, name: "Saúde", description: "Plano de saúde e remédios"},
             { id: 3, name: "Lazer", description: "Cinema, parques, praias, etc"},
             { id: 4, name: "Salário", description: "Recebimento de salário"},
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { catchError, flatMap } from 'rxjs/operators';
+import { catchError, flatMap, map } from 'rxjs/operators';
 
 import { Entry } from "./entry.model";
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from '../../../shared/services/base-resource.service';
+
+import * as moment from "moment"
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +27,14 @@ export class EntryService extends BaseResourceService<Entry>{
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+
+    // apenas para o in-memory
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     // gambi para o in-memory
     return this.categoryService.getById(entry.categoryId).pipe(
@@ -34,5 +44,15 @@ export class EntryService extends BaseResourceService<Entry>{
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 }
